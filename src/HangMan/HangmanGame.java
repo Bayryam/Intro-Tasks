87e01203612f2dcd456cd4fc87c68a14ffc8948c
Bayryam <bairqm.dermendzhiev123@gmail.com>
2023-09-14 16:44:53 +0300

    PasswordGenerator added

---
+++ src/HangMan/HangmanGame.java
@@ -0,0 +1,91 @@
+package HangMan;
+
+import Passwords.PasswordGenerator;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class HangmanGame {
+    private final static int MAX_WORD_LENGTH = 20;
+    private final static char HIDING_SYMBOL = '*';
+    private int lives = 8;
+    private  StringBuilder wordPresentation;
+    private final String wordToBeGuessed;
+
+
+    public HangmanGame() {
+        Random rand = new Random();
+        //digits as well, so we can reuse the generatePassword method
+        //we can transform it if needed
+        wordToBeGuessed = PasswordGenerator.generatePassword(rand.nextInt(MAX_WORD_LENGTH));
+        wordPresentation = new StringBuilder(wordToBeGuessed.length());
+        fillWordPresentation();
+
+    }
+
+    public void run() {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            startTurn();
+            String input = scanner.nextLine();
+
+            while (lives > 0) {
+
+                if (input.length() == 1) {
+
+                    if (wordToBeGuessed.contains(input))
+                        replaceGuessedSymbolInPresentation(input.charAt(0));
+                    else
+                        lives--;
+
+                    if (wordPresentation.indexOf(String.valueOf(HIDING_SYMBOL)) < 0) {
+                        System.out.println("Congratulations you guessed the word!");
+                        return;
+                    }
+                }
+                else
+                {
+                    if (wordToBeGuessed.compareTo(input) == 0) {
+                        System.out.println("Congratulations you guessed the word!");
+                        return;
+                    } else {
+                        lives--;
+                    }
+                }
+
+                startTurn();
+                input = scanner.nextLine();
+
+            }
+
+            System.out.println("Unluckily you lost the game!");
+        } catch
+        (Exception exception) {
+            //deal with exception
+
+        }
+    }
+
+
+    private void fillWordPresentation() {
+        for (int i = 0; i < wordPresentation.capacity(); i++) {
+            wordPresentation.append(HIDING_SYMBOL);
+        }
+    }
+
+    private void replaceGuessedSymbolInPresentation(char symbol) {
+        for (int i = 0; i < wordPresentation.length(); i++) {
+            if (wordToBeGuessed.charAt(i) == symbol) {
+                wordPresentation.replace(i, i + 1, String.valueOf(symbol));
+            }
+        }
+    }
+
+    private void startTurn(){
+        System.out.println(wordPresentation);
+        System.out.print("Enter the word or a letter: ");
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,17 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 
+import HangMan.HangmanGame;
+
+import Passwords.PasswordGenerator;
+
+import java.util.Random;
 
 public class Main {
 
-    public static String sumNumbers(String firstNumber, String secondNumber) {
-
-        StringBuilder result = new StringBuilder();
-        //int biggerLength = Math.max(firstNumber.length(), secondNumber.length());
-
-        int firstNumberIter = firstNumber.length() - 1;
-        int secondNumberIter = secondNumber.length() - 1;
-
-        int oneInMind = 0;
-        while (firstNumberIter >= 0 && secondNumberIter >= 0) {
-            int res = getDigitFromChar(firstNumber.charAt(firstNumberIter))
-                    + getDigitFromChar(secondNumber.charAt(secondNumberIter)) + oneInMind;
-            oneInMind = res / 10;
-            result.append(res % 10);
-
-            firstNumberIter--;
-            secondNumberIter--;
-        }
-
-        while (firstNumberIter >= 0) {
-            int res = getDigitFromChar(firstNumber.charAt(firstNumberIter)) + oneInMind;
-            oneInMind = res / 10;
-            result.append(res % 10);
-            firstNumberIter--;
-        }
-        while (secondNumberIter >= 0) {
-            int res = getDigitFromChar(secondNumber.charAt(secondNumberIter)) + oneInMind;
-            oneInMind = res / 10;
-            result.append(res % 10);
-            secondNumberIter--;
-        }
-
-        if (oneInMind == 1)
-            result.append(1);
-
-        return result.reverse().toString();
-    }
-
-
-
-    public static int getDigitFromChar(char symbol) {
-        return symbol - '0';
-    }
-
     public static void main(String[] args) {
-        System.out.println(sumNumbers("999", "2342342334"));
+        HangmanGame game = new HangmanGame();
+        game.run();
 
     }
 }
